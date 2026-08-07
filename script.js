3a6fefc065265d22afaa3e0e5124c5d8fb0c281b
ThisisVon <stevenbraxton428@gmail.com>
2026-08-06 23:38:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-d
+const button = document.getElementById("play");
+
+let playing = false;
+
+
+button.onclick = () => {
+
+  playing = !playing;
+
+
+  button.innerHTML =
+  playing ? "⏸" : "▶";
+
+};
